786d0a0a9dd25de3ae5efb0d8d5c7cb54219ed50
Luciane <lusarzi@gmail.com>
2022-03-19 16:16:49 -0300

    primeiro commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,19 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		/*int a = 2;
+		int b = 3;
+		System.out.println("Hello World" + (a+b));*/
+		
+		Gato gato=new Gato(null, null, null);
+		System.out.println(gato);
+	}
+
+}
+
